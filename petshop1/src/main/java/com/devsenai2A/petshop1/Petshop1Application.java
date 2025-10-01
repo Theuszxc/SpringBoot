e8eea1f95a2fbed709781505e645f91bec03d4e8
Theuszxc <matheuss.souza1a@gmail.com>
2025-10-01 07:13:13 -0300

    Add files via upload

---
+++ petshop1/src/main/java/com/devsenai2A/petshop1/Petshop1Application.java
@@ -0,0 +1,13 @@
+package com.devsenai2A.petshop1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Petshop1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Petshop1Application.class, args);
+	}
+
+}
---
+++ petshop1/src/test/java/com/devsenai2A/petshop1/Petshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenai2A.petshop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Petshop1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
